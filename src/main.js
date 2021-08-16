0cbf5131ebd44d28fce95859e542ff2a27637522
Josh <josh@bemoty.dev>
2021-08-16 20:55:34 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,132 @@
+const config = require("config");
+const log4js = require("log4js");
+const cron = require("node-cron");
+const SpotifyWebApi = require("spotify-web-api-node");
+const server = require("./server");
+
+log4js.configure({
+  appenders: {
+    console: { type: "console" },
+  },
+  categories: {
+    default: {
+      appenders: ["console"],
+      level: process.env.NODE_ENV !== "production" ? "debug" : "info",
+    },
+  },
+});
+
+const logger = log4js.getLogger("SpotifyApp");
+const spotifyApi = new SpotifyWebApi({
+  clientId: config.get("spotify.client_id"),
+  clientSecret: config.get("spotify.client_secret"),
+  redirectUri: config.get("spotify.redirect_uri"),
+});
+
+server(spotifyApi, async () => {
+  cron.schedule("0 0 * * *", () => {
+    logger.info("Running main loop");
+    mainLoop();
+  });
+  logger.info("Cronjob registered");
+  const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  readline.on("line", (input) => {
+    if (input.startsWith("reload")) {
+      let str = input.split(" ");
+      if (str.length === 1) {
+        mainLoop();
+        return;
+      }
+      logger.error("Illegal arguments");
+      return;
+    }
+    logger.error("Unknown command");
+  });
+});
+
+async function mainLoop() {
+  const data = await getSavedTracks();
+  logger.info(`Successfully loaded ${data.length} songs`);
+  await decorateArtistGenres(data);
+  logger.info(
+    `Populating ${config.get("spotify.playlists").length} playlist(s)`
+  );
+  for (let playlist of config.get("spotify.playlists")) {
+    logger.info(`Populating playlist '${playlist.name}'`);
+    const p = await spotifyApi.getPlaylist(playlist.id);
+    if (p.body.tracks.total > 0) {
+      await spotifyApi.removeTracksFromPlaylistByPosition(
+        playlist.id,
+        Array(p.body.tracks.total)
+          .fill()
+          .map((_, i) => i),
+        p.body.snapshot_id
+      );
+    }
+    const tracks = data.filter(
+      (t) => t.genres.filter((v) => playlist.genres.includes(v)).length !== 0
+    );
+    const chunkSize = 100;
+    const splitTracks = splitChunk(tracks, chunkSize);
+    let part = 0;
+    for (let split of splitTracks) {
+      logger.info(`Populating chunk ${part}-${part + chunkSize}...`);
+      await spotifyApi.addTracksToPlaylist(
+        playlist.id,
+        split.map((t) => t.track.uri)
+      );
+      part += chunkSize;
+    }
+  }
+  logger.info("Successfully completed main loop");
+}
+
+async function decorateArtistGenres(tracks) {
+  logger.info("Loading artist genre information...");
+  const artists = tracks
+    .map((a) => a.track.artists[0].id)
+    .filter((value, index, self) => self.indexOf(value) === index);
+  const chunkSize = 50;
+  let chunks = splitChunk(artists, chunkSize);
+  let artistData = [];
+  for (let chunk of chunks) {
+    logger.info(
+      `Loading artist chunk ${artistData.length}-${
+        artistData.length + chunkSize
+      }...`
+    );
+    const ad = await spotifyApi.getArtists(chunk);
+    artistData.push(...ad.body.artists);
+  }
+  for (let i = 0; i < tracks.length; i++) {
+    const artistId = tracks[i].track.artists[0].id;
+    const data = artistData.filter((a) => a.id === artistId);
+    tracks[i].genres = data[0].genres;
+  }
+  logger.info("Successfully loaded artist / genre data");
+}
+
+async function getSavedTracks(offset = 0) {
+  var data = await spotifyApi.getMySavedTracks({
+    offset: offset,
+  });
+  process.stdout.write("Loading tracks with offset: " + offset + "\r");
+  var tracks = data.body.items;
+  if (data.body.next) {
+    return (await getSavedTracks(offset + 20)).concat(tracks);
+  }
+  return tracks;
+}
+
+function splitChunk(arr, chunk) {
+  let arrOfArr = [];
+  while (arr.length > 0) {
+    let tempArray;
+    tempArray = arr.splice(0, chunk);
+    arrOfArr.push(tempArray);
+  }
+  return arrOfArr;
+}
---
+++ src/server.js
@@ -0,0 +1,73 @@
+const config = require("config");
+const log4js = require("log4js");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+
+const stateKey = "spotify_state_auth";
+
+function Server(spotifyApi, authorizedCallback) {
+  const app = express();
+  const logger = log4js.getLogger("Server");
+  app.use(cookieParser());
+  app.get("/login", (req, res) => {
+    const state = Math.random().toString(20).substr(2, 10);
+    res.cookie(stateKey, state);
+    res.redirect(
+      spotifyApi.createAuthorizeURL(config.get("spotify.scopes"), state)
+    );
+  });
+  app.get("/callback", (req, res) => {
+    const error = req.query.error;
+    const code = req.query.code;
+    const state = req.query.state;
+
+    if (error) {
+      logger.error("Callback error:", error);
+      res.send(`An error occurred while handling callback: ${error}`);
+      return;
+    }
+
+    const storedState = req.cookies ? req.cookies[stateKey] : null;
+    if (state === null || state !== storedState) {
+      res.send("A state mismatch occurred");
+      return;
+    }
+
+    spotifyApi
+      .authorizationCodeGrant(code)
+      .then((data) => {
+        const access_token = data.body["access_token"];
+        const refresh_token = data.body["refresh_token"];
+        const expires_in = data.body["expires_in"];
+
+        spotifyApi.setAccessToken(access_token);
+        spotifyApi.setRefreshToken(refresh_token);
+
+        logger.info(
+          `Successfully retrieved access token, will expire in ${expires_in}`
+        );
+        res.send("Success! You can now close this window");
+        server.close(); // don't need express anymore
+        setInterval(async () => {
+          const data = await spotifyApi.refreshAccessToken();
+          logger.info("The access token has been refreshed");
+          spotifyApi.setAccessToken(data.body["access_token"]);
+        }, (expires_in / 2) * 1000);
+        authorizedCallback();
+      })
+      .catch((err) => {
+        logger.error("Authorization error:", err);
+        res.send(`An error occurred while performing authorization: ${err}`);
+      });
+  });
+  const port = config.get("server.port");
+  const server = app.listen(port, () => {
+    logger.info(
+      `Initialized Spotify Web API, please authorize at ${config.get(
+        "server.host"
+      )}${port === "80" || port === "443" ? "" : `:${port}`}/login`
+    );
+  });
+}
+
+module.exports = Server;
